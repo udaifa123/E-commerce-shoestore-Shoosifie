5ea4dedf833b78e18d644642ffac8e0ff945a288
udaifa123 <udaifakk509@gmail.com>
2025-11-06 11:56:13 +0530

    fix: added lucide-react dependency

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
+  plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      external: ['sweetalert2', 'lucide-react'], // ✅ added lucide-react
+    },
+  },
 })
